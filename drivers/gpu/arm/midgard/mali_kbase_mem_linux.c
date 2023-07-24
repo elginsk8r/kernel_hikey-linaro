e52e6a3cf614551f69090dc82d7033cef11a636c
Tudor Ambarus <tudordana@google.com>
2023-07-24 12:50:53 +0000

    mali_kbase: fix build error due to removed vmas parameter from get_user_pages()
    upstream commit 54d020692b34 ("mm/gup: remove unused vmas parameter from get_user_pages()")
    removed unused vmas parameter and caused a build failure on mali_kbase
    driver. Get rid of the superfluous parameter.
    
    Fixes: 54d020692b34 ("mm/gup: remove unused vmas parameter from get_user_pages()")
    Bug: 292492156
    Change-Id: Ic3bc6fa8b520aaee17bf047cb016aadad5f0f23e
    Signed-off-by: Tudor Ambarus <tudordana@google.com>
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/gpu/arm/midgard/mali_kbase_mem_linux.c
+++ drivers/gpu/arm/midgard/mali_kbase_mem_linux.c
@@ -1110,10 +1110,14 @@ static struct kbase_va_region *kbase_mem_from_user_buffer(
 #elif LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0)
 	faulted_pages = get_user_pages(address, *va_pages,
 			reg->flags & KBASE_REG_GPU_WR, 0, pages, NULL);
-#else
+#elif LINUX_VERSION_CODE < KERNEL_VERSION(6, 4, 0)
 	faulted_pages = get_user_pages(address, *va_pages,
 			reg->flags & KBASE_REG_GPU_WR ? FOLL_WRITE : 0,
 			pages, NULL);
+#else
+	faulted_pages = get_user_pages(address, *va_pages,
+			reg->flags & KBASE_REG_GPU_WR ? FOLL_WRITE : 0,
+			pages);
 #endif
 
 	mmap_read_unlock(current->mm);
